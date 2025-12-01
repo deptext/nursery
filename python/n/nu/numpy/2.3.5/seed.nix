8104a732b7d1b5d487cf83588f0c5d84082f71ac
Craig Ulliott <craigulliott@gmail.com>
2025-12-01 15:58:47 -0500

    add python numpy 2.3.5

---
+++ python/n/nu/numpy/2.3.5/seed.nix
@@ -0,0 +1,13 @@
+{ deptext }:
+
+deptext.mkPythonPackage {
+  pname = "numpy";
+  version = "2.3.5";
+  hash = "sha256:04xwh16jm7szizkkhj637jv23i5x8jnzcfrw6bfsrssqkjykaxcm";
+  github = {
+    owner = "numpy";
+    repo = "numpy";
+    rev = "v2.3.5";
+    hash = "sha256:0qaz2mclr5cv3s5riag6aj3n3avirirnbi7sxpq4nw1vzrq09j6l";
+  };
+}
