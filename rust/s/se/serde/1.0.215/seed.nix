c17f97ea487dc833b0d38d54b7cb9d873ff23136
Craig Ulliott <craigulliott@gmail.com>
2025-12-01 19:34:34 -0500

    add rust serde 1.2.215

---
+++ rust/s/se/serde/1.0.215/seed.nix
@@ -0,0 +1,13 @@
+{ deptext }:
+
+deptext.mkRustPackage {
+  pname = "serde";
+  version = "1.0.215";
+  hash = "sha256:04xwh16jm7szizkkhj637jv23i5x8jnzcfrw6bfsrssqkjykaxcm";
+  github = {
+    owner = "serde-rs";
+    repo = "serde";
+    rev = "v1.0.215";
+    hash = "sha256:0qaz2mclr5cv3s5riag6aj3n3avirirnbi7sxpq4nw1vzrq09j6l";
+  };
+}
